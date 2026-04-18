f0aa16fb9a26cccca33eb65f4abfd47c2167d4f2
Ibrahim <eltabakh987@gmail.com>
2026-04-18 19:00:17 +0200

    change loader to scale loader

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { BarLoader, ScaleLoader } from "react-spinners";
+import { ScaleLoader } from "react-spinners";
 
 export default function Loading() {
-  return <BarLoader color="teal" />;
+  return <ScaleLoader color="teal" />;
 }
